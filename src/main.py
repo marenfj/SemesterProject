eb31b7ba5b3c8636cd649db2c1ec9d3ad7bc952f
marenfj <marenf.johansen@gmail.com>
2026-04-15 19:57:15 +0200

    wavmark watermarking of audio files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from watermarking import watermark_audios
+from wavmarkWatermarking import wavMark_watermark_audios
 from spectral_analysis import spectral_analysis
 
 def main():
-    watermark_audios()
-    spectral_analysis()
+    #watermark_audios()
+    wavMark_watermark_audios()
+   # spectral_analysis()
 
 if __name__ == "__main__":
     main()
---
+++ src/wavmarkWatermarking.py
@@ -0,0 +1,59 @@
+import numpy as np
+import soundfile
+import torch
+import torchaudio
+import wavmark
+from pathlib import Path
+
+output_directory = Path("wavMark_watermarked_audios")
+(output_directory / "bonafide").mkdir(parents=True, exist_ok=True)
+(output_directory / "spoofed").mkdir(parents=True, exist_ok=True)
+
+audiodir_bonafide = Path("datasets/ASVspoof5_partitions/audio_data/bonafide")
+audiodir_spoofed = Path("datasets/ASVspoof5_partitions/audio_data/spoofed")
+
+target_sample_rate = 16000
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = wavmark.load_model().to(device)
+
+payload = np.random.choice([0, 1], size=16)
+print("Payload:", payload)
+
+
+def watermark_bonafide_audio() -> None:
+    for audio_file_bonafide in audiodir_bonafide.iterdir():
+
+        wav, sr = torchaudio.load(str(audio_file_bonafide))
+        if sr != target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, target_sample_rate)
+            wav = resampler(wav)
+            sr = target_sample_rate
+
+        wav_bonafide = wav.mean(dim=0).numpy()
+        watermarked_bonafide, _ = wavmark.encode_watermark(model, wav_bonafide, payload, show_progress=True)
+        watermarked_bonafide_tensor = torch.from_numpy(watermarked_bonafide).unsqueeze(0)
+
+        output_path = output_directory / "bonafide" / f"{audio_file_bonafide.stem}_watermarked.flac"
+        torchaudio.save(str(output_path), watermarked_bonafide_tensor, sr)
+
+
+def watermark_spoofed_audio() -> None:
+    for audio_file_spoofed in audiodir_spoofed.iterdir():
+
+        wav, sr = torchaudio.load(str(audio_file_spoofed))
+        if sr != target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, target_sample_rate)
+            wav = resampler(wav)
+            sr = target_sample_rate
+
+        wav_spoofed = wav.mean(dim=0).numpy()
+        watermarked_spoofed, _ = wavmark.encode_watermark(model, wav_spoofed, payload, show_progress=True)
+        watermarked_spoofed_tensor = torch.from_numpy(watermarked_spoofed).unsqueeze(0)
+
+        output_path = output_directory / "spoofed" / f"{audio_file_spoofed.stem}_watermarked.flac"
+        torchaudio.save(str(output_path), watermarked_spoofed_tensor, sr)
+
+def wavMark_watermark_audios() -> None:
+    watermark_bonafide_audio()
+    watermark_spoofed_audio()
