4803220af505098b7b096af734d0c7e4bce276ad
marenfj <marenf.johansen@gmail.com>
2026-05-21 19:31:09 +0200

    frequency robustness analysis

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from wavmarkWatermarking import wavMark_watermark_audios
 from spectral_analysis import spectral_analysis
 from detection import run_detection
 from detection_metrics import detection_metrics
+from robustness import run_robustness
 
 def main():
-    watermark_audios()
-    wavMark_watermark_audios()
-    spectral_analysis()
-    run_detection()
-    detection_metrics()
+    #watermark_audios()
+    #wavMark_watermark_audios()
+    #spectral_analysis()
+    #run_detection()
+    #detection_metrics()
+    run_robustness()
 
 if __name__ == "__main__":
     main()
